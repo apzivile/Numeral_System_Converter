09dcda51f82e3f2655c9fd0b9b442a6ba45de1ec
apzivile <zivile.apo@gmail.com>
2020-06-28 11:53:14 +0100

    Numeral System Converter; last 5th and 6th parts finished

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -2,27 +2,89 @@ package converter;
 
 import java.util.Scanner;
 
+class Num {
+    long intPart;
+    double fracPart;
+    String intString;
+    String fracString;
+    boolean isToOne;
+    boolean isWithFrac;
+}
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int sourceRadix = sc.nextInt();
-        String numInString = sc.next();
-        int targetRadix = sc.nextInt();
-
-        long num;
-        if (sourceRadix == 1) {
-            num = numInString.length();
+        int radixOfNum = 0;
+        String numInString = "";
+        int radixTarget=0;
+        if (sc.hasNextInt()) {
+            radixOfNum = sc.nextInt();
         } else {
-            num = Long.parseLong(numInString, sourceRadix);
+            System.out.println("error");
         }
-
-        if (targetRadix == 1) {
-            for (int i = 0; i < num; i++) {
-                System.out.print(1);
-            }
+        if (sc.hasNext()) {
+            numInString = sc.next();
+        }else {
+            System.out.println("error");
+        }
+        if (sc.hasNextInt()) {
+            radixTarget = sc.nextInt();
+        }else {
+            System.out.println("error");
+        }
+        if (radixOfNum < 1 || radixOfNum > 36 || radixTarget < 1 || radixTarget > 36) {
+            System.out.println("error");
         } else {
-            String numTarget = Long.toString(num, targetRadix);
-            System.out.print(numTarget);
+            int roundUpFive = 5;
+            Num num = new Num();
+
+            if (numInString.contains(".")) {
+                num.isWithFrac = true;
+                int point = numInString.indexOf(".");
+                num.intString = numInString.substring(0, point);
+                num.fracString = numInString.substring(point + 1);
+            } else {
+                num.intString = numInString;
+            }
+
+            if (radixOfNum == 1) {
+                num.intPart = num.intString.length();
+            } else {
+                num.intPart = Long.parseLong(num.intString, radixOfNum);
+            }
+
+            if (radixTarget == 1) {
+                num.isToOne = true;
+                for (int i = 0; i < num.intPart; i++) {
+                    System.out.print(1);
+                }
+            } else {
+                String intTarget = Long.toString(num.intPart, radixTarget);
+                System.out.print(intTarget);
+            }
+
+            if (!num.isToOne && num.isWithFrac) {
+                char[] digits = num.fracString.toCharArray();
+                StringBuilder fracTarget = new StringBuilder();
+                fracTarget.append(".");
+
+                for (int i = 0; i < digits.length; i++) {
+                    double partOfFrac =
+                            Character.digit(digits[i], radixOfNum)
+                                    / Math.pow(radixOfNum, i + 1);
+                    num.fracPart += partOfFrac;
+                }
+
+                for (int i = 0; i < roundUpFive; i++) {
+                    double fracToTargetRadix = num.fracPart * radixTarget;
+                    long intPartOfFrac = (long) fracToTargetRadix;
+                    String intPartOfFracStr = Long.toString(intPartOfFrac, radixTarget);
+                    num.fracPart = fracToTargetRadix - intPartOfFrac;
+                    fracTarget.append(intPartOfFracStr);
+                }
+
+                System.out.print(fracTarget.toString());
+            }
         }
     }
 }
