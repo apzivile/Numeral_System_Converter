827a603dc4f49008b94126b8bfc12d9ecd593939
apzivile <zivile.apo@gmail.com>
2020-06-28 11:31:43 +0100

    Numeral System Converter; 3rd and 4th part finished

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -4,9 +4,25 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int number = scanner.nextInt();
-        System.out.println(number % 8);
+        Scanner sc = new Scanner(System.in);
+        int sourceRadix = sc.nextInt();
+        String numInString = sc.next();
+        int targetRadix = sc.nextInt();
+
+        long num;
+        if (sourceRadix == 1) {
+            num = numInString.length();
+        } else {
+            num = Long.parseLong(numInString, sourceRadix);
+        }
+
+        if (targetRadix == 1) {
+            for (int i = 0; i < num; i++) {
+                System.out.print(1);
+            }
+        } else {
+            String numTarget = Long.toString(num, targetRadix);
+            System.out.print(numTarget);
+        }
     }
 }
-
